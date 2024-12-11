c30dc6a292630291abf3924d33bf9f32d5884529
Volodymyr Andriienko <vandrjios@gmail.com>
2024-12-11 19:52:34 +0200

    Revert "[core] ObservationIgnored removed."
    This reverts commit 3d5e96f753a71b369a60c8ca1b533f63f12c1859.

--- Sources/VADefaultsMacros/UserDefaultsData.swift
+++ Sources/VADefaultsMacros/UserDefaultsData.swift
@@ -25,8 +25,9 @@ public struct UserDefaultsData: MemberMacro, MemberAttributeMacro {
               !(variableDeclSyntax.isStaticVariable || variableDeclSyntax.isClassVariable) else {
             return []
         }
+        let isObservable = declaration.as(ClassDeclSyntax.self)?.attributes.contains(where: { $0.as(AttributeSyntax.self)?.attributeName.identifier == "Observable" }) ?? false
         guard !variableDeclSyntax.attributes.isDefaultsValueMacro else {
-            return []
+            return isObservable ? ["@ObservationIgnored"] : []
         }
         guard variableDeclSyntax.bindings.count == 1,
             !variableDeclSyntax.bindings.contains(where: {
@@ -35,9 +36,16 @@ public struct UserDefaultsData: MemberMacro, MemberAttributeMacro {
             return []
         }
 
-        return [
-            "@\(raw: String(describing: DefaultsValue.self))",
-        ]
+        return if isObservable {
+            [
+                "@ObservationIgnored",
+                "@\(raw: String(describing: DefaultsValue.self))",
+            ]
+        } else {
+            [
+                "@\(raw: String(describing: DefaultsValue.self))",
+            ]
+        }
     }
 
     public static func expansion(
